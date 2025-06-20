3849cf87ff966241be359bbf35612f747246d4b0
giamimino <miminoshvili2102@example.com>
2025-06-20 23:33:00 +0400

    header & welcome section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,32 @@
 import React, { useEffect, useState } from 'react';
 import styles from './page.module.scss'
 import { Counting } from '@/components/animations/animations';
+import Image from 'next/image';
+import { Icon } from '@iconify/react';
 
 export default function Home() {
   
   return (
     <div className="p-24">
       <section className={styles.heroWelcome}>
-        <h1 className='font-extrabold text-6xl'>Buy your dream plants</h1>
         <aside>
+          <h1 className='font-extrabold text-6xl w-[400px]'>Buy your dream plants</h1>
           <div className='flex'>
             <Counting end={50} content="Plant Species" style={{borderRight: "1px solid black", paddingLeft: "0"}} />
             <Counting end={100} content="Costumers" />
           </div>
+          <div className={styles.search}>
+            <input type="text" placeholder='What are you looking for?'/>
+            <span>
+              <Icon icon="lets-icons:search"  />
+            </span>
+          </div>
         </aside>
+        <div>
+          <Image src="https://raw.githubusercontent.com/giamimino/images/refs/heads/main/greenland/plant.webp" 
+          width={411}
+          height={513} alt='plant'/>
+        </div>
       </section>
     </div>
   );
